572b96f81f242bcd074c0ce83181eddb1e469fbc
Azat Dovgeldiyev <58405201+adovgeldiyev@users.noreply.github.com>
2020-04-08 19:26:08 -0500

    init

--- Instructions/starter_code/api_keys.py
+++ Instructions/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "2743272bc529410c30ad64564062fd66"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBwFQsqpu54n95bX4ZArGrBh5-CViDUzjk"
